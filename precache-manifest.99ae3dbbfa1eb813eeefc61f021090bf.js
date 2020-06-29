5382d96092b7de76ca502c6d0cf494aa82cf3ac1
binmulyadin <sarana.automation@gmail.com>
2020-06-29 21:22:05 +0700

    deploy

---
+++ precache-manifest.99ae3dbbfa1eb813eeefc61f021090bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a22a9bc181290e4350ba74e40d3c58f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54fa1dc8ef33e911dfa9",
+    "url": "/static/css/main.cd241259.chunk.css"
+  },
+  {
+    "revision": "5b6d830537e4cad1fff0",
+    "url": "/static/js/2.aa668122.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.aa668122.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54fa1dc8ef33e911dfa9",
+    "url": "/static/js/main.1798b0db.chunk.js"
+  },
+  {
+    "revision": "823694c2996e4c11d31a",
+    "url": "/static/js/runtime-main.d279a6a0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
